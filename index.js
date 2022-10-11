3a1ddbfe51ddb21eea5ec1d430b6298fbecb5e83
apigeek22 <ravenhedden@google.com>
2022-10-11 18:33:28 -0500

    Got it working using async

--- index.js
+++ index.js
@@ -11,40 +11,48 @@ const apigee = apigeejs.apigee;
 /**
   * A basic "hello world" type endpoint
 */
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   console.log('Starting Job');
 
-  // First get environment variables
+  // First get important variables
+  // Potential improvement is to bring in google-auth module
+  // to print token on the fly
+  const proxyObj = {};
   const options = {
     org : process.env.org,
-    user: process.env.username,
-    password: process.env.password,
-    token: process.env.token,
+    token: process.env.token, // gcloud auth print-access-token
     apigeex: true
   };
 
-  // 
+  try {
+    // First connect to Apigee
+    const org = await apigee.connect(options);
 
-  // Connect to Apigee
-  const org = apigee.connect(options).then ( org => {
-    console.log(`Connected to org`);
-    // First list all API Proxies in environment
+    // Next get a list of all API Proxies & map to proxyObj
+    const proxies = await org.proxies.get();
+    for (const proxy of proxies.proxies) {
+      proxyObj[proxy.name] = [];
+    }
 
-    // Then list all API Products and their associated API Proxies
+    // Then get all API Products and map to the APIs
+    const products = await org.products.get({expand:true});
+    for (const product of products.apiProduct) {
+      const productName = product.name;
 
-    // Map product data to full list of API Proxies
+      // Get apis within
+      for (const prodApi of product.operationGroup.operationConfigs) {
+        const prodApiName = prodApi.apiSource;
+        proxyObj[prodApiName].push(productName);
+      }
+    }
 
-    // Return data
-    org.get().then(data => {
-      res.send(data);
-    }).catch((err) => {
-      console.error(err);
-      res.send("Unable to list API proxies and products");
-    });
-  }).catch((err) => {
-    console.error(err);
-    res.send("Unable to list API proxies and products");
-  });
+    // Return data to client
+    console.log('Job complete');
+    res.send(proxyObj);
+  } catch (e) {
+    console.error(e);
+    res.send('Error: Unable to retreive data');
+  }
 });
 
 const port = parseInt(process.env.PORT) || 8080;
